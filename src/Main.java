2ebbf9c32683c04eee9845e99399b15b487306d7
 Eвгений Костерин <qsterin@gmail.com>
2022-07-21 19:43:10 +0300

    Удалил из кода все лишнее

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,8 @@ public class Main {
         long var_num;
         System.out.print("Введите сумму пополнения счета: ");
         var_num = ticket.nextInt();
-        if (var_num > 100) {
             System.out.print("Ваш баланс: " + (var_num + x));
             System.out.print("\nКоличество бонусных баллов на счету: " + var_num/100);
         }
-        else {System.out.print("Ваш баланс: " + (var_num + x));
-            System.out.print("\nКоличество бонусных баллов на счету: " + var_num/100);}
-    }
+
 }
