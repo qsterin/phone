d48189e82b975ee8ecea3edf685e55632ed937a5
 Eвгений Костерин <qsterin@gmail.com>
2022-07-27 08:50:28 +0300

    Исправил переменные. Поправил условие рассчета

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int x = 100;
+        int initialAmount = 100;
         Scanner ticket = new Scanner(System.in);
-        long var_num;
+        long replenishmentAmount;
         System.out.print("Введите сумму пополнения счета: ");
-        var_num = ticket.nextInt();
-        if (var_num >= 100) {;
-            long y = var_num/100;
-            System.out.print("Ваш баланс: " + (var_num + x + y));
-            System.out.print("\nКоличество бонусных баллов на счету: " + var_num / 100);
+        replenishmentAmount = ticket.nextInt();
+        if (replenishmentAmount >= 101) {;
+            long bonuses = replenishmentAmount/100;
+            System.out.print("Ваш баланс: " + (replenishmentAmount + initialAmount + bonuses));
+            System.out.print("\nКоличество бонусных баллов на счету: " + replenishmentAmount / 100);
         }
         else {;
-            System.out.print("Ваш баланс: " + (var_num + x ));
-            System.out.print("\nКоличество бонусных баллов на счету: " + var_num / 100);
+            System.out.print("Ваш баланс: " + (replenishmentAmount + initialAmount ));
+            System.out.print("\nКоличество бонусных баллов на счету: " + 0);
         }
     }
 }
